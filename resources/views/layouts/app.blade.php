17c1f826e507eb16d1279a9f7143b6ee411f92a3
ITD MAGANG 02 <itd.magang02@aiia.co.id>
2026-04-17 10:16:39 +0700

    clear code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>
-            {{-- {{ config('app.name', 'Laravel') }} --}}
+            {{-- {{ config(in'app.name', 'Laravel') }} --}}
             {{ config('app.name') }} - @yield('title')
         </title>
 
